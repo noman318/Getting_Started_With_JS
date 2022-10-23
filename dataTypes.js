9518a742f8871c0073379de3679e9e5dba998010
Shaikh Mohd Noman <74728455+noman318@users.noreply.github.com>
2022-10-23 19:54:09 +0530

    Completed till all types of DataTypes

---
+++ dataTypes.js
@@ -0,0 +1,28 @@
+// NN BB SS U
+// N Null
+// N Number
+// B Boolean
+// B BigInt
+// S String
+// S Symbol
+// U Undefined
+
+let a = null
+let b = 89
+let c = true
+let d = BigInt('567') + BigInt('3')
+let e = 'Noman'
+let f = Symbol('My Name is Noman')
+let g
+
+// console.log(a, b, c, d, e, f, g)
+// console.log(typeof d)
+
+// Objects in Js
+const item = {
+    "my": true,
+    'very': 'false',
+    "educat": 90,
+    "lang": undefined
+}
+console.log(item['my'])
