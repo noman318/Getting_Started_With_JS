1239bb4d84823ed0b851926871fe3b2510f36a06
Shaikh Mohd Noman <74728455+noman318@users.noreply.github.com>
2022-10-23 19:17:55 +0530

    Learned till variables

---
+++ first.js
@@ -0,0 +1,14 @@
+let b = 89
+// console.log(a)
+b="john"
+// var k =90
+// const k =78
+// console.log(a)
+{
+    let b = 'this'
+    console.log(b)
+    // let b = true
+    // console.log(b)
+}
+console.log(b)
+// console.log(k)
