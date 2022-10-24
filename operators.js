f7a82bb2e42f7019fa33d1d61af69f09f5b772a6
Shaikh Mohd Noman <74728455+noman318@users.noreply.github.com>
2022-10-24 11:18:55 +0530

    Completed all types of operators

---
+++ operators.js
@@ -0,0 +1,65 @@
+// Operators in Js
+// Arithmatic Operators
+
+let a = 16
+let b = 2
+// console.log("a + b = ",a+b)
+// console.log("a - b = ",a-b)
+// console.log("a * b = ",a*b)
+// console.log("a / b = ",a/b)
+// console.log("a ** b = ",a**b)
+// console.log("a % b = ",a%b)
+// console.log('a++', a++) a++ increases the values of a by 1 after
+// console.log('++b', ++b) ++b increases the values of b by 1 before
+// console.log('b--', b--)
+// console.log('--b', --b)
+// console.log(++a)
+// console.log(a++)
+// console.log(--a)
+// console.log(a--)
+// console.log(a)
+// console.log(a--)
+// console.log(a)
+
+// console.log(b)
+
+// Assignments Operators
+
+// let c = 1
+// c += 5
+// console.log(c)
+
+// c -= 2
+// console.log(c)
+
+// c *= 5
+// console.log(c)
+
+// c /= 5
+// console.log(c)
+
+// Comparison Operators
+
+// let comp1 =3
+// let comp2 = 4
+
+// console.log("Comparing values ",comp1 == comp2)
+// console.log("Comparing values ",comp1 != comp2)
+// console.log("Comparing values ",comp1 === comp2)
+// console.log("Comparing values ",comp1 !== comp2)
+
+// let comp1 = 3
+// let comp2 = '3'
+
+// console.log("Comparing values ",comp1 == comp2)
+// console.log("Comparing values ",comp1 != comp2)
+// console.log("Comparing values ",comp1 === comp2)
+// console.log("Comparing values ",comp1 !== comp2)
+
+// Logical Operators
+let log = 4
+let log1 = 6
+// console.log('Logical Comparing : ', log<log1 && log==4)
+console.log('Logical Comparing : ', log<log1 || log==90)
+console.log(!true)
+console.log(!false)
