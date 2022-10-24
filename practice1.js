09d21d2ae5c95c54b115ae3d50f7597f85e9e99a
Shaikh Mohd Noman <74728455+noman318@users.noreply.github.com>
2022-10-24 09:39:26 +0530

    Completed Practice 1

---
+++ practice1.js
@@ -0,0 +1,26 @@
+let a = "My age is : "
+let b = 22;
+// console.log(a+b)
+// console.log(typeof (a+b))
+
+const item = {
+    name: 'Noman',
+    section: 1,
+    isAdmin: false,
+}
+// item = "Shaikh"
+item['Friend'] = 'Found'
+item['name'] = 'Shaikh_Noman'
+console.log(item)
+
+const dict = {
+    'nominal': 'being something in name only but not in reality',
+    'smug': 'too pleased of yourselves',
+    'assertive': 'express your opinion clearly and firmly so that people understand you or do what you want',
+    'self-pity': 'the state of thinking too much about your own problems or troubles and feeling sorry about yourself',
+    'self-esteem': 'a ggod opinon of our own character and abilities',
+    'ego': 'the opinion that you have for yourself',
+    'attitude': 'the way you think, feel or behave'
+}
+console.log(dict.attitude)
+console.log(dict["self-pity"])
