12e7977d3cc21a96355e68b4f640d2beeed018e4
Shaikh Mohd Noman <74728455+noman318@users.noreply.github.com>
2022-11-03 00:01:24 +0530

    Basics of Array

---
+++ array.js
@@ -0,0 +1,17 @@
+console.log('MyArray')
+let marks = [82,74,80,77,68]
+marks[5]=78 ;
+// marks[6]=85 ;
+// console.log(marks)
+console.log(marks.length)
+for(let i =0; i<marks.length ; i++){
+    console.log(marks[i])
+}
+// console.log(typeof marks)// Array is a type of Object
+// console.log(marks[0])
+// console.log(marks[1])
+// console.log(marks[2])
+// console.log(marks[3])
+// console.log(marks[4])
+// console.log(marks[5]) // It wil print undefined beacuse there no element at index[9]
+// Arrays are mutable and arrays can be changed
