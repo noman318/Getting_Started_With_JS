5ad26591b2a9197916206a9e52ff31f37cf32021
Shaikh Mohd Noman <74728455+noman318@users.noreply.github.com>
2022-11-03 00:13:42 +0530

    Basic Array Methods done

---
+++ arrayMethod.js
@@ -0,0 +1,12 @@
+let num = [23,45,67,89,59,89]
+// let b = num.toString()
+// console.log(b)
+// let c = num.join('_')
+// console.log(c)
+// num.pop() // removes the last element of the array
+// let d =num.push(67)
+// console.log(num)
+// let r = num.shift()
+// console.log(r, num)
+// let q = num.unshift(45)
+console.log(num)
