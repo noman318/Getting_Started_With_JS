de3209dae63dacad478980f64fb1e2df6e3e9e09
noman318 <shaikhmohdnoman612@gmail.com>
2022-11-10 11:27:09 +0530

    Basic DOM and BOM done

---
+++ htmlCssJsWindow/index.js
@@ -0,0 +1,5 @@
+console.log("Getting_started_Window_obj_DOM_BOM");
+console.log(document.body);
+// document.body.style.backgroundColor = "aqua";
+console.log("Bowser_Object_Model");
+location.href = "https://github.com/";
