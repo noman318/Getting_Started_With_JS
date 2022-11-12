0e2eaf9360b584d1d248453b168fa41aa2d32831
Shaikh Mohd Noman <74728455+noman318@users.noreply.github.com>
2022-11-12 09:57:30 +0530

    minor tests

--- htmlCssJsWindow/script.js
+++ htmlCssJsWindow/script.js
@@ -8,3 +8,16 @@
 
 let color = prompt('Enter any color u want: ')
 document.body.style.background = color;
+
+
+// function firstOne(){
+//     console.log('This is the first function created')
+// }
+// function thirdOne(){
+//     console.log('This is the Third function created')
+// }
+// setTimeout(() =>{
+//     console.log('This is the Second function created')
+//     setTimeout(thirdOne, 2000)
+// }, 5000)
+// firstOne()
