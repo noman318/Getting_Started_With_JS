420c28e2535966b43888bc8f6c761e2e0911f489
Shaikh Mohd Noman <74728455+noman318@users.noreply.github.com>
2022-11-13 16:20:46 +0530

    DOM Search

---
+++ DomSearch/index.js
@@ -0,0 +1,13 @@
+let ctitle = document.getElementById('first')
+ctitle.style.color='red'
+let ctitles = document.querySelectorAll('.card-title')
+ctitles[0].style.color='red'
+ctitles[1].style.color='green'
+ctitles[2].style.color='blue'
+console.log(ctitles)
+document.querySelector('.thiss').style.color='gray'
+document.querySelector('.thiss').style.background='black'
+
+console.log(document.getElementsByTagName('a'))
+console.log(document .querySelector('.card').getElementsByTagName('a'))
+console.log(document.getElementsByName('search'))
