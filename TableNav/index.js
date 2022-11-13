7b20fdac76671c96c0a0a446422ca01b632bc6ed
Shaikh Mohd Noman <74728455+noman318@users.noreply.github.com>
2022-11-13 15:34:16 +0530

    Table Based Navigation

---
+++ TableNav/index.js
@@ -0,0 +1,6 @@
+let t = document.body.firstElementChild.firstElementChild
+console.log(t)
+console.log(t.rows)
+console.log(t.rows[2].rowIndex)
+console.log(t.tHead.firstElementChild)
+console.log(t.tBodies)
