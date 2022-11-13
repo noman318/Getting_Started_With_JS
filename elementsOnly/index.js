ab0b293adb395b5a4c3ae0612ebd334c81c5e9cc
Shaikh Mohd Noman <74728455+noman318@users.noreply.github.com>
2022-11-13 11:25:07 +0530

    Acessing parents and child nodes of html via js

---
+++ elementsOnly/index.js
@@ -0,0 +1,9 @@
+const changeCol =()=>{
+    document.body.firstElementChild.style.background = 'red'
+}
+
+
+let b = document.body
+console.log(b.firstChild)
+console.log(b.firstElementChild)
+// console.log(b.nextElementSibling)
